c15eb61cb79efcd7d2866a080d9bf7498eb52405
Dima <dimakurinnoy8@gmail.com>
2024-12-22 21:31:00 +0100

    test test test

--- src/components/ConnectHeaderMenu/ConnectHeaderMenu.tsx
+++ src/components/ConnectHeaderMenu/ConnectHeaderMenu.tsx
@@ -160,15 +160,7 @@ const SolanaConnectionManagerElement = () => {
   //     )}
   //   </div>
   // );
-  const LABELS = {
-    'change-wallet': 'Change wallet',
-    connecting: 'Connecting ...',
-    'copy-address': 'Copy address',
-    copied: 'Copied',
-    disconnect: 'Disconnect',
-    'has-wallet': 'Connect',
-    'no-wallet': 'Connect SOL',
-} as const;
+ 
 
   return isMobile() ? (
     <div className={style.connect_content}>
